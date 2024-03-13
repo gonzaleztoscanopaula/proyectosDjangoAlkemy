d6071c1d96dc8920bbcc332d0b862534a9c8bdf4
Paula González toscano <gonzaleztoscano.pau@gmail.com>
2024-03-13 15:10:36 -0300

    "probando json"

--- clase10comi32/miApp/urls.py
+++ clase10comi32/miApp/urls.py
@@ -4,5 +4,7 @@ from. import views
 urlpatterns = [
     path("", views.crear_usuario),
     path("all", views.mostrar_usuarios),
-    path("filter", views.filtrar_usuario)
+    path("filter/<int:edad>", views.filtrar_usuario),
+    path("update/<int:id>", views.actualizar_usuario),
+    path("json",views.ejemplo_json_view)
     ]
--- clase10comi32/miApp/views.py
+++ clase10comi32/miApp/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from .models import Usuario
+from django.http import JsonResponse
 
 # Create your views here.
 #create
 def crear_usuario(request):
     nuevoUsuario = Usuario.objects.create(
-        nombre = "Juan",
+        nombre = "Marcos",
         apellido = "Perez",
         edad = 30
     )    
     return render(request,"usuarios.html",{"nuevoUsuario": nuevoUsuario})
+
+#JSON Response
+def ejemplo_json_view(request):
+    #Crea un diccionario con los datos que quieres devolver en formato JSON
+    data={"mensaje":"Hola desde json response!", "numero":42 }
+    #Crea una respuesta JSON
+    #return JsonResponse(data)
+    return render(request,"json.html",{"data": data})
+
 
 #read
 
@@ -20,3 +30,10 @@ def mostrar_usuarios(request):
 def filtrar_usuario(request):
     datosFiltrados =   Usuario.objects.filter(edad=25)
     return render(request,"usersFiltrados.html",{"datosFiltrados": datosFiltrados})
+
+#update
+def actualizar_usuario(request, id):
+    usuarioActualizar = Usuario.objects.get(id=id)
+    usuarioActualizar.edad = 25
+    usuarioActualizar.save()
+    return render(request,"updatedUser.html",{"updatedUser": usuarioActualizar})
